d77c8c825faa83b038049f726e93fa45e06fccf5
witships <witships@gmail.com>
2024-02-06 23:31:48 +0900

    sass導入

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
+<script>
+	import '../app.scss';
+</script>
+
 <nav>
 	<a href="/">トップ</a>
 	<a href="/about">アバウト</a>
 </nav>
 
-<slot />
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	nav {
+		padding: 1rem;
+	}
+	main {
+		padding: 1rem;
+	}
+</style>
