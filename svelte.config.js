48601d29fc124c0ee81cc34eb56bdc7bcfa30c35
witships <witships@gmail.com>
2024-02-07 21:34:26 +0900

    scss調整

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,16 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // 追加
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	},
-// 	preprocess: [vitePreprocess()] // 追加
-// };
-
-// export default config;
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // 追加
 import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
 		})
-	}
+	},
+	preprocess: [vitePreprocess()] // 追加
 };
